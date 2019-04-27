044f8a99f6210ec34d0e18a54f66927944fd5aa3
Kendy Nguyen <kendyisnguyen@gmail.com>
2019-04-27 14:56:39 -0700

    refactor project component

--- src/components/project.js
+++ src/components/project.js
@@ -1,12 +1,13 @@
 import React from "react"
+import ProjectButtons from "./projectButtons"
 
-const mainColor = `#8c8aff`
 const headerColor = `#343b4d`
 const accentColor = `#596482`
 
 const Project = props => (
   <div
     style={{
+      backgroundColor: `white`,
       boxShadow: `0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)`,
       borderRadius: `1rem`,
       display: `grid`,
@@ -54,51 +55,7 @@ const Project = props => (
           ))}
         </ul>
       </div>
-      <div
-        className="buttons"
-        style={{
-          display: `flex`,
-          justifyContent: `space-around`,
-          marginTop: `0.75rem`,
-        }}
-      >
-        <button
-          type="submit"
-          style={{
-            height: `2.75rem`,
-            padding: `0 1rem`,
-            borderRadius: `.35rem`,
-            color: `#fff`,
-            backgroundColor: `${mainColor}`,
-            fontWeight: 600,
-            textTransform: `uppercase`,
-            lineHeight: `2rem`,
-            letterSpacing: `.04rem`,
-            boxShadow: `0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)`,
-            cursor: `pointer`,
-          }}
-        >
-          Source Code
-        </button>
-        <button
-          type="submit"
-          style={{
-            backgroundColor: `transparent`,
-            height: `2.75rem`,
-            padding: `0 1rem`,
-            borderRadius: `.35rem`,
-            color: `${mainColor}`,
-            fontWeight: 600,
-            textTransform: `uppercase`,
-            lineHeight: `2rem`,
-            letterSpacing: `.04rem`,
-            boxShadow: `0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)`,
-            cursor: `pointer`,
-          }}
-        >
-          Live Demo
-        </button>
-      </div>
+      <ProjectButtons />
     </div>
   </div>
 )
